425a9b5a6048ed71c4e32fa875393df38f7fb83a
Fahin <abirlucario02@gmail.com>
2025-09-03 16:02:17 +0600

    adding

---
+++ dunder/sc1.py
@@ -0,0 +1,9 @@
+def fav(artist):
+    print(artist)
+def main():
+    print("This is sc1")
+    print(__name__)
+    fav("Green Day")
+if __name__ == "__main__":
+    main()
+    
---
+++ dunder/sc2.py
@@ -0,0 +1,4 @@
+from sc1 import *
+print("This is sc2")
+print(__name__)
+fav("Alec Benjamin")
