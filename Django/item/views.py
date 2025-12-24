967244c69f2f2a3117dc21903d182c6a08b2c924
Kazi Fahin Abraj Abir <2405007@ugrad.cse.buet.ac.bd>
2025-12-25 00:25:49 +0600

    Remove deprecated Django core and item modules, including migrations, templates, and media files.

--- Django/item/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-
-from core.models import Item
-
-def detail(request, pk):
-    item=get_object_or_404(Item, pk=pk)
-    return render(request, 'item/detail.html', {
-        'item': item
-    })
